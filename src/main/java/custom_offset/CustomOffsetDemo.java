defca40bf0cda9221225a6f2fddd8e0417e14430
官国成 <guanguocheng365@gmail.com>
2019-08-12 19:35:40 +0800

    自定义存储offset

---
+++ src/main/java/custom_offset/CustomOffsetDemo.java
@@ -0,0 +1,63 @@
+package custom_offset;
+
+import org.apache.kafka.clients.consumer.*;
+import org.apache.kafka.common.TopicPartition;
+import org.apache.kafka.common.serialization.StringDeserializer;
+
+import java.util.*;
+
+public class CustomOffsetDemo {
+    private static Map<TopicPartition, Long> currentOffset = new HashMap<>();
+
+    public static void main(String[] args) {
+        Properties props = new Properties();
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "ggc:9092");
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, "CustomConsumerTest");
+        props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, false);
+
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<>(props);
+        consumer.subscribe(Collections.singletonList("first"), new ConsumerRebalanceListener() {
+
+            //该方法会在Rebalance之前调用
+            @Override
+            public void onPartitionsRevoked(Collection<TopicPartition> partitions) {
+                System.out.println("ConsumerRebalanceListener#onPartitionsRevoked called ==> " + partitions);
+                commitOffset(currentOffset);
+            }
+
+            //该方法会在Rebalance之后调用
+            @Override
+            public void onPartitionsAssigned(Collection<TopicPartition> partitions) {
+                System.out.println("ConsumerRebalanceListener#onPartitionsAssigned called ===> " + partitions);
+                currentOffset.clear();
+                for (TopicPartition partition : partitions) {
+                    consumer.seek(partition, getOffset(partition));//定位到最近提交的offset位置继续消费
+                }
+            }
+        });
+
+        while (true) {
+            ConsumerRecords<String, String> records = consumer.poll(100);//消费者拉取数据
+
+            //tx.begin
+            for (ConsumerRecord<String, String> record : records) {
+                System.out.printf("offset = %d, key = %s, value = %s%n", record.offset(), record.key(), record.value());
+                currentOffset.put(new TopicPartition(record.topic(), record.partition()), record.offset());
+            }
+            commitOffset(currentOffset);
+            //tx.commit
+        }
+    }
+
+    //获取某分区的最新offset
+    private static long getOffset(TopicPartition partition) {
+        return 0;
+    }
+
+    //提交该消费者所有分区的offset
+    private static void commitOffset(Map<TopicPartition, Long> currentOffset) {
+
+    }
+}
